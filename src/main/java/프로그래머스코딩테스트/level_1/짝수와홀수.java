1d732d99b5344b28d75c0c8842a3dbfa736d1c0f
gorhkd <gorhkd223@gmail.com>
2024-12-08 16:52:51 +0900

    sosososo easy

---
+++ src/main/java/프로그래머스코딩테스트/level_1/짝수와홀수.java
@@ -0,0 +1,9 @@
+package 프로그래머스코딩테스트.level_1;
+
+public class 짝수와홀수 {
+    class Solution {
+        public String solution(int num) {
+            return (num % 2 == 0) ? "Even" : "Odd" ;
+        }
+    }
+}
